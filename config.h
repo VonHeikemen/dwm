babe78106022b62663795fdc5d501922625f8a73
Heiker Curiel <heikercuriel@gmail.com>
2019-07-13 18:01:52 -0400

    config

--- config.h
+++ config.h
@@ -65,12 +65,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[]  = { "firefox-developer-edition", NULL };
+static const char *filemanagercmd[]  = { "pcmanfm", NULL };
+static const char *musicplayercmd[]  = { "kitty", "-e", "tmux", "new-session", "-A", "-D", "-s", "music", "sh -c 'cmus'", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_F2,     spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_F3,     spawn,          {.v = filemanagercmd } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = musicplayercmd } },
 
 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
