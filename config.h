3ded234b68412323e74ae90caf91941fe1d67388
Heiker Curiel <heikercuriel@gmail.com>
2019-07-13 21:58:34 -0400

    config

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -9,17 +9,18 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_black[]       = "#242830";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_black,  col_cyan  },
 };
 
 /* tagging */
@@ -93,6 +94,9 @@ static Key keys[] = {
   // Close an application
   { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 
+  // Exit dwm
+  { MODKEY,                       XK_Escape, quit,           {0} },
+
   // Toggle bar
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 
@@ -111,8 +115,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      focusstack,     {.i = -1 } },
 
   // Resize windows
-  { MODKEY|ShiftMask,                XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY|ShiftMask,                XK_l,      setmfact,       {.f = +0.05} },
+  { MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
+  { MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
 
   // Move windows
 	{ MODKEY|ControlMask,           XK_i,      incnmaster,     {.i = +1 } },
@@ -125,7 +129,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-  { MODKEY,                       XK_Escape, quit,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
